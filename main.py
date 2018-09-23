31854ad61f6a1e229befbece30a15d5cda0e9b99
RyujiTamaki <ryuji.tamaki@ousia.jp>
2018-09-24 01:20:04 +0900

    2. Entity, Property, Query を使ってみる

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ For more information, see README.md
 """
 
 # [START all]
+import os
 import cgi
 import textwrap
 import urllib
@@ -28,7 +29,16 @@ import urllib
 from google.appengine.ext import ndb
 
 import webapp2
+import jinja2
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class Book(ndb.Model):
+    name = ndb.StringProperty()
 
 # [START greeting]
 class Greeting(ndb.Model):
@@ -43,62 +53,54 @@ class Greeting(ndb.Model):
         return cls.query(ancestor=ancestor_key).order(-cls.date)
 
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write('<html><body>')
-        guestbook_name = self.request.get('guestbook_name')
+class BookPage(webapp2.RequestHandler):
+    def get(self, guestbook_name):
         ancestor_key = ndb.Key("Book", guestbook_name or "*notitle*")
         greetings = Greeting.query_book(ancestor_key).fetch(20)
 # [END query]
-
-        greeting_blockquotes = []
-        for greeting in greetings:
-            greeting_blockquotes.append(
-                '<blockquote>%s</blockquote>' % cgi.escape(greeting.content))
-
-        self.response.out.write(textwrap.dedent("""\
-            <html>
-              <body>
-                {blockquotes}
-                <form action="/sign?{sign}" method="post">
-                  <div>
-                    <textarea name="content" rows="3" cols="60">
-                    </textarea>
-                  </div>
-                  <div>
-                    <input type="submit" value="Sign Guestbook">
-                  </div>
-                </form>
-                <hr>
-                <form>
-                  Guestbook name:
-                    <input value="{guestbook_name}" name="guestbook_name">
-                    <input type="submit" value="switch">
-                </form>
-              </body>
-            </html>""").format(
-                blockquotes='\n'.join(greeting_blockquotes),
-                sign=urllib.urlencode({'guestbook_name': guestbook_name}),
-                guestbook_name=cgi.escape(guestbook_name)))
+        template_values = {
+            'guestbook_name': guestbook_name,
+            'greetings': greetings
+        }
+        template = JINJA_ENVIRONMENT.get_template('book.html')
+        self.response.write(template.render(template_values))
 
 
 # [START submit]
 class SubmitForm(webapp2.RequestHandler):
-    def post(self):
-        # We set the parent key on each 'Greeting' to ensure each guestbook's
-        # greetings are in the same entity group.
-        guestbook_name = self.request.get('guestbook_name')
+    def post(self, guestbook_name):
         greeting = Greeting(parent=ndb.Key("Book",
                                            guestbook_name or "*notitle*"),
                             content=self.request.get('content'))
         greeting.put()
 # [END submit]
-        self.redirect('/?' + urllib.urlencode(
-            {'guestbook_name': guestbook_name}))
+        self.redirect('/books/' + guestbook_name)
+
+
+class AddBook(webapp2.RequestHandler):
+    def post(self):
+        guestbook_name = self.request.get('guestbook_name')
+        book = Book(id=guestbook_name,
+                    name=guestbook_name)
+        book.put()
+        self.redirect('/')
+
+
+class BookList(webapp2.RequestHandler):
+    def get(self):
+        books_query = Book.query().order(Book.name)
+        books = books_query.fetch()
+        template_values = {
+            'books': books
+        }
+        template = JINJA_ENVIRONMENT.get_template('booklist.html')
+        self.response.write(template.render(template_values))
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/sign', SubmitForm)
+    webapp2.Route('/', handler=BookList, name='BookList'),
+    webapp2.Route('/add_book', handler=AddBook),
+    webapp2.Route('/books/<guestbook_name>', handler=BookPage),
+    webapp2.Route('/books/<guestbook_name>/post', handler=SubmitForm)
 ])
 # [END all]
